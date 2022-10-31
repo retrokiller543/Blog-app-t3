8a9548d303562678c402f9bf2a0af49c6eac4cce
retrokiller543 <emil.schutt@gmail.com>
2022-10-31 10:41:13 +0100

    Added HIBP verification meta-data

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Box } from './components/box'
+import Head from 'next/head'
 
 type LayoutProps = {
   children: React.ReactNode
@@ -8,16 +9,24 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <main className="w-screen">
-      <Box
-        css={{
-          maxW: '100vw',
-        }}
-      >
-        <Header />
-        <section className="flex items-center ">{children}</section>
-        <Footer />
-      </Box>
-    </main>
+    <>
+      <Head>
+        <meta
+          name="have-i-been-pwned-verification"
+          content="f06da9c064307eaf1ca76508d5301b75"
+        />
+      </Head>
+      <main className="w-screen">
+        <Box
+          css={{
+            maxW: '100vw',
+          }}
+        >
+          <Header />
+          <section className="flex items-center ">{children}</section>
+          <Footer />
+        </Box>
+      </main>
+    </>
   )
 }
