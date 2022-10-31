8a17edff4074efc9effffa19bf0daaaa508f06fd
retrokiller543 <emil.schutt@gmail.com>
2022-10-31 10:45:27 +0100

    Header dont work :(

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,7 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Box } from './components/box'
-import Head from 'next/head'
 
 type LayoutProps = {
   children: React.ReactNode
@@ -9,24 +8,16 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <>
-      <Head>
-        <meta
-          name="have-i-been-pwned-verification"
-          content="f06da9c064307eaf1ca76508d5301b75"
-        />
-      </Head>
-      <main className="w-screen">
-        <Box
-          css={{
-            maxW: '100vw',
-          }}
-        >
-          <Header />
-          <section className="flex items-center ">{children}</section>
-          <Footer />
-        </Box>
-      </main>
-    </>
+    <main className="w-screen">
+      <Box
+        css={{
+          maxW: '100vw',
+        }}
+      >
+        <Header />
+        <section className="flex items-center ">{children}</section>
+        <Footer />
+      </Box>
+    </main>
   )
 }
